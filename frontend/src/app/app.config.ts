af771ca3a4be15e0978c4ff8ef476ad8d7ac020b
Tóth Bence <tothbence0531@gmail.com>
2026-01-16 22:47:58 +0100

    frontend: navbar, home page hero

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,75 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { definePreset } from '@primeuix/themes';
+
+const mainPreset = definePreset(Aura, {
+  semantic: {
+    colorScheme: {
+      light: {
+        surface: {
+          0: '#ffffff',
+          50: '{neutral.50}',
+          100: '{neutral.100}',
+          200: '{neutral.200}',
+          300: '{neutral.300}',
+          400: '{neutral.400}',
+          500: '{neutral.500}',
+          600: '{neutral.600}',
+          700: '{neutral.700}',
+          800: '{neutral.800}',
+          900: '{neutral.900}',
+          950: '{neutral.950}',
+        },
+      },
+      dark: {
+        surface: {
+          0: '#ffffff',
+          50: '{neutral.50}',
+          100: '{neutral.100}',
+          200: '{neutral.200}',
+          300: '{neutral.300}',
+          400: '{neutral.400}',
+          500: '{neutral.500}',
+          600: '{neutral.600}',
+          700: '{neutral.700}',
+          800: '{neutral.800}',
+          900: '{neutral.900}',
+          950: '{neutral.950}',
+        },
+      },
+    },
+    primary: {
+      50: '#f7f4ef',
+      100: '#eae2d7',
+      200: '#d7c6b1',
+      300: '#bfa485',
+      400: '#ac8763',
+      500: '#9c7454',
+      600: '#875e47',
+      700: '#6d483b',
+      800: '#5d3f36',
+      900: '#513732',
+      950: '#2e1d1a',
+    },
+  },
+});
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: mainPreset,
+        options: {
+          darkModeSelector: '.dark-mode',
+        },
+      },
+    }),
+  ],
 };
